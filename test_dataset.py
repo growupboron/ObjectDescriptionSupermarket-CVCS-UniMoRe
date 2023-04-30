2c865e6c4d44835ad3c3af8665ef1d41d55c23fd
MicheleBellitti <michelebellitti272@gmail.com>
2023-04-30 02:34:38 +0200

    e

--- test_dataset.py
+++ test_dataset.py
@@ -82,7 +82,7 @@ with torch.no_grad():
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
     accuracy = 100 * correct / total
-    print('Accuracy of the network on the test images: %d {}' % accuracy)
+    print(f'Accuracy of the network on the test images: {accuracy}%')
 
 
 # save the model
