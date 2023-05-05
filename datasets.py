91c770ea70e53a143cda39084c7adc867c98f24b
MicheleBellitti <michelebellitti272@gmail.com>
2023-05-06 00:18:55 +0200

    minor changes

--- datasets.py
+++ datasets.py
@@ -22,13 +22,12 @@ mean = (0.485, 0.456, 0.406)
 std = (0.229, 0.224, 0.225)
 # Define transforms
 TRANSFORM = transforms.Compose([
-    transforms.Resize(256),  # resize the image to 256x256 pixels
-    transforms.CenterCrop(224),  # crop the image to 224x224 pixels around the center
+    transforms.Resize(224),  # resize the image to 256x256 pixels
     transforms.ToTensor(),  # convert the image to a PyTorch tensor
-    # transforms.Normalize(mean=mean, std=std),  # normalize the image
+    transforms.Normalize(mean=mean, std=std),  # normalize the image
     transforms.GaussianBlur(kernel_size=(5, 5)),
     transforms.RandomHorizontalFlip(p=0.5),  #
-    transforms.RandomRotation(degrees=180),  # data augmentation
+    #transforms.RandomRotation(degrees=180),  # data augmentation
     transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.05),
 
     # see https://pytorch.org/docs/stable/torchvision/transforms.html for more transforms
